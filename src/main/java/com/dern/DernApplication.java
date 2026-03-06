6bc9c808b0463613c40bda34159abb443c520bf8
swathy-1327 <swathy2006malu@gmail.com>
2026-03-06 15:39:16 +0530

    initialization

---
+++ src/main/java/com/dern/DernApplication.java
@@ -0,0 +1,13 @@
+package com.dern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DernApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dern/DernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
